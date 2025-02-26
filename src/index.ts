0615ea8d8704bd8465c66547ba1e0b84fb5bffcc
AceOfBens <aceofbens@gmail.com>
2025-02-26 07:30:24 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
-var run = await Bot.run(getPostText { dryRun: true }); // Run the bot, supplying it the getPostText function from getPostText.ts.
+var run = await Bot.run(getPostText { dryRun: false }); // Run the bot, supplying it the getPostText function from getPostText.ts.
 console.log(`[${new Date().toISOString()}] Posts complete!`); // Write to the console, saying that the posting operation is complete.
